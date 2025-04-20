0fe8fba4fd111d833c0b74f059724ca5f81af8a0
PaulSommers <psommers1@ivytech.edu>
2025-04-20 07:46:12 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def hello_world():
+    print("Hello, World!")
+
+def calculate_sum(a,b):
+	return a+b
+
+def multiply(a, b):
+  return a * b
+
+if __name__ == "__main__":
+    hello_world()
+    result = calculate_sum(5, 10)
+    print(f"Sum: {result}")
+    product = multiply(5, 10)
+    print(f"Product: {product}")
